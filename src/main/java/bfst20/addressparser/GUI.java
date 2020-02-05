f6a9c54cc47331614b18b7665c08f1c3f6767661
Thomas Hoffmann Kilbak <thhk@itu.dk>
2020-02-05 19:23:11 +0100

    Update GUI.java

--- src/main/java/bfst20/addressparser/GUI.java
+++ src/main/java/bfst20/addressparser/GUI.java
@@ -2,9 +2,12 @@ package bfst20.addressparser;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -12,24 +15,39 @@ import javafx.stage.Stage;
  * GUI
  */
 public class GUI extends Application {
+    Controller c = new Controller();
+    public static Label lastSearch = new Label();
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
+        Model model = new Model();
+
+        var hBox = new HBox();
         var pane = new BorderPane();
         var input = new TextField();
         var output = new TextArea();
-        input.setOnAction(e -> {
-            var raw = input.getText();
-            var parsed = Address.parse(raw);
-            input.clear();
-            output.appendText(parsed + "\n\n");
-        });
+
+        var vBox = new VBox();
+
+        output.setEditable(false);
+        c.loadAddress(input,output);
         input.setFont(new Font(30));
         output.setFont(new Font(30));
-        pane.setTop(input);
         pane.setCenter(output);
-        primaryStage.setTitle("Awesomest Address Aligner 2.0");
-        primaryStage.setScene(new Scene(pane));
+        primaryStage.setTitle("Tjoms' seje program xDDD gaming");
+
+        vBox.getChildren().addAll(new Label("Søgehistorik:"),pane);
+
+        VBox vBox1 = new VBox();
+        lastSearch.setFont(new Font(30));
+
+        vBox1.getChildren().addAll(input,new Label("Seneste resultat:"),lastSearch);
+
+        hBox.getChildren().addAll(vBox1,vBox);
+
+
+        primaryStage.setScene(new Scene(hBox));
         primaryStage.show();
     }
+
 }
